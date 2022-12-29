78f18aad4c2dc7e328ddf651e3d73ab4e147208e
joanaalemao <j.alemao.developer@gmail.com>
2022-12-29 14:36:40 +0000

    Init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import PokemonCard from '../components/PokemonCard'
+
+export const Home = () => {
+  return (
+    
+  <div><Navbar/>
+  <PokemonCard/></div>
+  )
+}
