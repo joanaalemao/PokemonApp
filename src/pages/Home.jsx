eb0f612fc291b41c8a16ab1514f0af08e6a1d453
joanaalemao <j.alemao.developer@gmail.com>
2023-01-01 20:30:08 +0000

    Axios - API get method
    get method to https://pokeapi.co/

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,24 @@
 import { Grid } from '@mui/material'
 import { Container } from '@mui/system'
+import axios from 'axios'
 import React from 'react'
 import Navbar from '../components/Navbar'
 import PokemonCard from '../components/PokemonCard'
 
 export const Home = () => {
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+    .then((res) => console.log(res))
+    .catch((error)  => console.log(error));
   return (
 <div><Navbar/>
 <Container maxWidth="xg">
 <Grid container>
-  <PokemonCard/>
-   
+  <PokemonCard/>     
+<PokemonCard></PokemonCard>
+  <PokemonCard></PokemonCard>
     </Grid></Container>
+    <Container><Grid container><PokemonCard></PokemonCard></Grid></Container>
   </div>
   )
 }
