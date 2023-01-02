7777a6a7bd234b8f1916676615bc5540e5f6a930
joanaalemao <j.alemao.developer@gmail.com>
2023-01-02 12:42:41 +0000

    Added Function
    getPokemons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Grid } from '@mui/material'
 import { Container } from '@mui/system'
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import PokemonCard from '../components/PokemonCard'
 
-export const Home = () => {
-  axios
+export const Home = () => {   useEffect(()=>{getPokemons()}   )
+const getPokemons=() =>{ } 
+    axios
     .get("https://pokeapi.co/api/v2/pokemon?limit=50")
     .then((res) => console.log(res))
     .catch((error)  => console.log(error));
